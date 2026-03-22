c34b73f53a564a859d159e33065a49600f035d39
Marcus <marcus.fonseca968@gmail.com>
2026-03-22 14:02:14 -0300

    Adição da lógica de exibição e exclusão de numeros - display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Linkado")
+const result = document.getElementById("result");
+const button1 = document.getElementById("button1").innerText;
+const plus = document.getElementById("plus");
+
+function display(num) {
+  result.innerHTML += `${num}`;
+}
+
+function backspace() {
+  result.innerHTML = result.innerText.slice(0, -1);
+}
+
+function sum() {
+    
+}
